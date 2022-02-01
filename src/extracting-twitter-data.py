a29841014fd2f9870ce5d59b2fb56bfad43021d4
Nayef Ahmad <nayef.ahmad.11@gmail.com>
2022-01-31 17:18:21 -0800

    tweepy demo

--- src/extracting-twitter-data.py
+++ src/extracting-twitter-data.py
@@ -11,4 +11,17 @@ api = tweepy.API(auth)
 
 print(f"API Host: {api.host}")
 
-# tweets = tweepy.Cursor(api.search, )
+# public_tweets = api.home_timeline()
+# for tweet in public_tweets:
+#     print(tweet.text)
+
+# search_term = 'bayes'
+# tweets = tweepy.Cursor(api.search_30_day(label='tweets', query=search_term))
+
+# retreived_tweets = [tweet.text for tweet in tweets]
+# df = pd.json_normalize(retreived_tweets)
+
+# tweets = tw.Cursor(api.search,
+#               q=search_query,
+#               lang="en",
+#               since="2020-09-16").items(50)
