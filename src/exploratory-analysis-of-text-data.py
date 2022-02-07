3ffd83957783e587b9841882a2ea58f73ccd3f72
Nayef Ahmad <nayef.ahmad.11@gmail.com>
2022-02-07 09:32:02 -0800

    EDA on UN speech data

--- src/exploratory-analysis-of-text-data.py
+++ src/exploratory-analysis-of-text-data.py
@@ -1,6 +1,22 @@
-import matplotlib.pyplot as plt
-import seaborn as sns
+# # Analyzing the UN General Assembly Debates data
+#
+#
 
-tips = sns.load_dataset("tips")
-sns.lmplot(x="total_bill", y="tip", hue="smoker", data=tips)
-plt.show()
+# ## Imports
+import pandas as pd
+from pathlib import Path
+from src.utils import get_repo_path
+
+# ## Loading data
+dir = Path(get_repo_path(), "data")
+file = "un-general-debates.csv"
+df_un_debates = pd.read_csv(dir.joinpath(file))
+
+df_un_debates.head(5)
+df_un_debates.sample(5)
+
+df_un_debates.columns
+df_un_debates.dtypes
+df_un_debates.info()
+df_un_debates.describe().T
+# df_un_debates.describe(include=['0'])  # todo: include='0' should let us include categorical data  # noqa
--- src/utils.py
+++ src/utils.py
@@ -4,3 +4,8 @@ import yaml
 def read_yaml(path):
     with open(path, "r") as f:
         return yaml.safe_load(f)
+
+
+def get_repo_path():
+    config = read_yaml(r"./config.yaml")
+    return config["root_path"]
